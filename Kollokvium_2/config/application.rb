ef758f7dd0d77ad8e3b943cb98edcfdbad407a33
Alexander Sakovich <sashasakovich06@gmail.com>
2026-01-07 15:59:37 +0300

    Add CI

--- Kollokvium_2/config/application.rb
+++ Kollokvium_2/config/application.rb
@@ -29,6 +29,8 @@ module Kollokvium2
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.sass.quiet_deps = true
+
     config.api_only = true
   end
 end
